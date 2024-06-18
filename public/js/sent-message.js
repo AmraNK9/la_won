c76031dc4c8f23046a9e16d30f22d59b931d5c89
kim <naymyothat.mmtcompany@gmail.com>
2024-06-18 20:27:08 +0630

    finish v1

--- public/js/sent-message.js
+++ public/js/sent-message.js
@@ -1,28 +1,55 @@
+
+
 const input = document.getElementById("message-input");
 const rightChat = document.getElementById("right-chat");
+const leftChat = document.getElementById("left-chat");
 const sentButton = document.getElementById("sent-btn");
+const socket = io('http://localhost:3001');
+const messagesDiv = document.getElementById('messages');
+const messageInput = document.getElementById('messageInput');
 
 console.log(input)
 
-input.addEventListener('change',(e)=>{
+input.addEventListener('change', (e) => {
     // console.log(e.target.value);
 
     // createMessageBox(e.target.value)
-    
-    createMessageBox(input.value)
-    rightChat.appendChild(createMessageBox(input.value))
+    sendMessage(e.target.value)
+    // rightChat.appendChild(createMessageBox(input.value))
     input.value = ''
 })
 
-sentButton.addEventListener('click',(e)=>{
+sentButton.addEventListener('click', (e) => {
 
-
+    sendMessage(e.target.value)
+    // rightChat.appendChild(createMessageBox(input.value))
+    input.value = ''
 });
 
 
 
+socket.on('message', (msg) => {
+    let spawnPos = [leftChat, rightChat];
 
-function createMessageBox(message){
+    const spawnIndex = Math.random() * 2; // This will give a number between 0 and 2 (exclusive of 2)
+    console.log(spawnIndex.toFixed() -1);
+
+
+
+
+    spawnPos[spawnIndex.toFixed()-1].appendChild(createMessageBox(msg))
+
+
+});
+
+function sendMessage(msg) {
+    socket.emit('message', msg);
+}
+
+
+
+
+function createMessageBox(message) {
     const messageBox = document.createElement('div');
 
     messageBox.classList.add('message');
